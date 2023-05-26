dce618604f6b0a9ea91d1076cff002e71e73058b
devfraga <devfraga@gmail.com>
2023-05-26 14:09:32 -0400

    Projeto youtube header animado com react native

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
-import { View, Animated, StatusBar, Text, FlatList, StyleSheet } from "react-native";
+import { View, StatusBar, Text, FlatList, StyleSheet, Animated, Image } from "react-native";
+import { useRef } from 'react'
+
+const H_MAX_HEGIHT = 150;
+const H_MIN_HEIGHT = 50;
+const H_SCROLL_DISTANCE = H_MAX_HEGIHT - H_MIN_HEIGHT;
 
 const App = () => {
   const data = [
@@ -8,24 +13,54 @@ const App = () => {
     { id: '4', title: 'Item 4' },
   ];
 
+  const scrollOffsetY = useRef(new Animated.Value(0)).current;
+
+  const headerScrollHeight = scrollOffsetY.interpolate({
+    inputRange: [0, H_SCROLL_DISTANCE],
+    outputRange: [H_MAX_HEGIHT, H_MIN_HEIGHT],
+    extrapolate: 'clamp'
+  })
+
+  const imageScaleHeight = scrollOffsetY.interpolate({
+    inputRange: [0, 150],
+    outputRange: [80, 34],
+    extrapolate: 'clamp'
+  })
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar backgroundColor={"#121212"} barStyle="light-content" translucent={false} />
       
       <Animated.View
         style={{
-          width: "100%",
-          height: 150,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 99,
+          width: '100%',
+          height: headerScrollHeight,
           padding: 10,
           backgroundColor: "#121212",
           alignItems: 'center',
-          justifyContent: 'center'
+          justifyContent: 'center',
+          overflow: 'hidden'
         }}
         >
-          <Text style={styles.headerText}>Meu Header</Text>
+          
+          <Animated.Image
+            source={require("./src/logo.png")}
+            style={{
+              width: 80,
+              height: imageScaleHeight,
+            }}
+            resizeMode="contain"
+          />
+
       </Animated.View>
 
       <FlatList
+        style={{ paddingTop: H_MAX_HEGIHT }}
         data={data}
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
@@ -34,6 +69,11 @@ const App = () => {
             <Text>{item.title}</Text>
           </View>
         )}
+
+        onScroll={Animated.event([
+          { nativeEvent: { contentOffset: { y: scrollOffsetY } } },
+        ], { useNativeDriver: false } )}
+        scrollEventThrottle={16}
       />
 
     </View>
